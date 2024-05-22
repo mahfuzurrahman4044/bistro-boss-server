8de29fb10dba97b1d68182bf5075d3d7121714cb
Mahfuzur Rahman <119561643+mahfuzurrahman4044@users.noreply.github.com>
2024-05-22 23:09:23 +0600

    some update

--- index.js
+++ index.js
@@ -3,8 +3,28 @@ const app = express();
 const cors = require("cors");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+var jwt = require('jsonwebtoken');
 app.use(express.json());
 app.use(cors());
+
+const verifyJwt = (req, res, next) => {
+  const authorization = req.headers.token;
+
+  if (!authorization) {
+    res.status(401).send({ error: true, message: "Unauthorized Access" })
+  }
+
+  const token = authorization.split(" ")[1]
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ error: true, message: "Forbidden Access" })
+    }
+
+    req.decoded = decoded;
+    next()
+  });
+}
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.83ramik.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -35,6 +55,12 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send("Bistro Boss Restaurant");
 });
+
+app.post("/jwt", (req, res) => {
+  const user = req.body
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: "1h" });
+  res.send({ token })
+})
 
 const nodeMailer = require("nodemailer");
 
@@ -90,13 +116,19 @@ app.post("/carts", async (req, res) => {
   res.send(result);
 })
 
-app.get("/carts", async (req, res) => {
+app.get("/carts", verifyJwt, async (req, res) => {
   const result = await cartsCollection.find().toArray()
   res.send(result)
 })
 
 app.get("/carts/:email", async (req, res) => {
   const email = req.params.email;
+
+  // const decodedEmail = req.decoded.email
+  // if (email !== decodedEmail) {
+  //   return res.status(403).send({ error: true, message: "Invalid Token" })
+  // }
+
   const result = await cartsCollection.find({ email: email }).toArray()
   res.send(result)
 })
@@ -138,11 +170,27 @@ app.get("/users", async (req, res) => {
 
 app.patch("/users/admin/:id", async (req, res) => {
   const id = req.params.id;
+  console.log(id)
   const filtler = { _id: new ObjectId(id) }
 
   const updateDoc = {
     $set: {
       role: "admin"
+    },
+  };
+
+  const result = await usersCollection.updateOne(filtler, updateDoc)
+  res.send(result)
+})
+
+app.patch("/users/seller/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id)
+  const filtler = { _id: new ObjectId(id) }
+
+  const updateDoc = {
+    $set: {
+      role: "seller"
     },
   };
 
